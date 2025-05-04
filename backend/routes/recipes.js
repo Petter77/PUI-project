392ce1bbc48f84af928d023c2bcf1ee8cad8b145
maciej790 <maciejmw126@gmail.com>
2025-05-04 15:10:07 +0200

    done favourite recipes backend

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -9,7 +9,7 @@ router.post('/save', (req, res) => {
   const checkSql = 'SELECT * FROM favourite_recipes WHERE ApiRecipeID = ? AND UserID = ?';
 
   db.query(checkSql, [recipeId, userId], (err, results) => {
-    if (err) return res.status(500).json({ error: 'Błąd serwera przy sprawdzaniu przepisu.' });
+    if (err) return res.status(500).json({ error: 'Błąd serwera!' });
 
     if (results.length > 0) {
       return res.status(409).json({ message: 'Ten przepis już został dodany do ulubionych!' });
@@ -24,5 +24,44 @@ router.post('/save', (req, res) => {
     });
   });
 });
+
+router.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.user.userId;
+
+  const checkSql = 'SELECT * FROM favourite_recipes WHERE ApiRecipeID = ? AND UserID = ?';
+
+  db.query(checkSql, [id, userId], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Błąd serwera!' });
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Przepis nie został znaleziony.' });
+    }
+
+    const deleteSql = 'DELETE FROM favourite_recipes WHERE UserID = ? AND ApiRecipeID = ?';
+
+    db.query(deleteSql, [userId, id], (err, result) => {
+      if (err) return res.status(500).json({ error: 'Błąd serwera przy usuwaniu przepisu.' });
+
+      return res.status(200).json({ message: 'Przepis został usunięty pomyślnie!' });
+    });
+  });
+});
+
+router.get('/saved-recipes', (req, res) =>{
+  const userId = req.user.user.userId;
+  const getSql = 'SELECT * FROM favourite_recipes WHERE UserID = ?';
+
+  db.query(getSql, [userId], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Błąd serwera!' });
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Brak zapisanych przepisów.' });
+    }
+    return res.status(200).json(results)
+  })
+
+
+})
 
 module.exports = router;
